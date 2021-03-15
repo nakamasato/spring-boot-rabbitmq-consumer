de3bab5a74445cfdbddfb771695dce56e2906077
masatonaka <masatonaka1989@gmail.com>
2021-03-15 22:15:33 +0900

    謎にエラーも出ないけど反応もない

--- src/main/java/com/naka/springbootrabbitmqconsumer/SpringBootRabbitmqConsumerApplication.java
+++ src/main/java/com/naka/springbootrabbitmqconsumer/SpringBootRabbitmqConsumerApplication.java
@@ -1,18 +1,40 @@
 package com.naka.springbootrabbitmqconsumer;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
-import com.rabbitmq.client.DeliverCallback;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class SpringBootRabbitmqConsumerApplication {
+
+
+	static final String topicExchangeName = "spring-boot-exchange";
+
+	static final String queueName = "spring-boot";
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange(topicExchangeName);
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	}
+
 
 	@Bean
 	SimpleMessageListenerContainer container(MessageListenerAdapter listenerAdapter) {
